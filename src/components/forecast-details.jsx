629ff614b8130be03cbfe4573e7bc25473d4e355
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-03-08 18:38:56 +0000

    updated css

--- src/components/forecast-details.jsx
+++ src/components/forecast-details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import '../styles/forecast-details.css';
 
 const ForecastDetails = forecast => {
     return (<div className="detailedForecast" >
